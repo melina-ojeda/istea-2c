055c196eb0263ceafab25588a2e192abb1ae9cff
Melina Ojeda <melina.ojeda@istea.com.ar>
2025-10-26 23:56:24 -0300

    new component products-container

---
+++ app.js
@@ -0,0 +1,6 @@
+let products = document.getElementById('products-container'); // elemento padre
+
+let newProduct = document.createElement('div');
+newProduct-setAtribute("class", "product-item")
+
+products.appendChild(newProduct); // de esta manera lo agregamos al dom
