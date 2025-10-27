40595b365887b74a3f5d0b2f8a385765ffb73681
Melina Ojeda <melina.ojeda@istea.com.ar>
2025-10-27 01:40:23 -0300

    new design in products page

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
-let products = document.getElementById('products-container'); // elemento padre
+const products = document.getElementById('products-container'); // elemento padre
 
-let newProduct = document.createElement('div');
+// creo los elementos
+const newProduct = document.createElement('div');
 newProduct-setAtribute("class", "product-item")
 
-products.appendChild(newProduct); // de esta manera lo agregamos al dom
+const newAnchor = document.createElement('a');
+newAnchor.setAttribute("href", "/product-detail.html");
+
+const newDiv = document.createElement('div');
+newDiv.setAttribute("class", "product-image");
+
+// agrego el contenido nuevo al dom 
+products.appendChild(newProduct); 
