1f71a5654697aac4a95d48c38c356a0ca5eab7b0
Melina Ojeda <melina.ojeda@istea.com.ar>
2025-10-28 21:24:57 -0300

    add new product using js

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
-const products = document.getElementById('products-container'); // elemento padre
-
 // creo los elementos
 const newProduct = document.createElement('div');
-newProduct-setAtribute("class", "product-item")
+newProduct.setAttribute("class", "product-item");
 
 const newAnchor = document.createElement('a');
 newAnchor.setAttribute("href", "/product-detail.html");
 
-const newDiv = document.createElement('div');
-newDiv.setAttribute("class", "product-image");
+const newImage = document.createElement('img');
+newImage.setAttribute("src", "./img/item-gomitas.jpg");
+newImage.setAttribute("alt", "producto nuevo");
+
+const newProductName = document.createElement('p');
+newProductName.textContent = "Producto nuevo";
+
+const newProductPrice = document.createElement('span');
+newProductPrice.textContent = "$Precio";
+
+// estructura
+newProduct.appendChild(newAnchor);
+newAnchor.appendChild(newImage);
+newProduct.appendChild(newProductName);
+newProduct.appendChild(newProductPrice);
 
 // agrego el contenido nuevo al dom 
-products.appendChild(newProduct); 
+const productsContainer = document.querySelector('.products-grid'); // elemento padre
+productsContainer.appendChild(newProduct); 
+
+
